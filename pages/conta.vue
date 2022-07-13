fc200a0339e553ec22ea2ba183fe5339437d3af2
orsanor <orsanor@gmail.com>
2022-07-13 18:19:16 -0300

    att-geral

--- pages/conta.vue
+++ pages/conta.vue
@@ -1,41 +1,73 @@
 <template>
   <v-app>
-    <v-main>
+    <v-main class="page">
       <div>
-        <v-row>
-          <v-col cols="6">
-            <v-img
-              class="ma-5 mt-2"
-              max-height="100%"
-              max-width="18%"
-              src="https://blog.bancoafro.com.br/wp-content/themes/blogbancoafro/img/logo/logo.png"
-            >
-            </v-img>
-          </v-col>
-          <v-col cols="6" class="justify-center">
-            <v-btn
-              class="rounded-xl mt-5 float-right mr-3 pa-1"
-              elavation="2"
-              to="/"
-            >
-              Sair
-            </v-btn>
-          </v-col>
-        </v-row>
+        <v-card height="100px" width="auto%">
+          <v-row>
+            <v-col cols="6">
+              <v-img
+                class="ma-5 mt-2"
+                max-height="auto"
+                max-width="18%"
+                src="https://blog.bancoafro.com.br/wp-content/themes/blogbancoafro/img/logo/logo.png"
+              >
+              </v-img>
+            </v-col>
+            <v-col cols="6" class="justify-center">
+              <v-btn
+                class="rounded-xl mt-5 float-right mr-3 pa-1"
+                elavation="1"
+                to="/"
+              >
+                Sair
+              </v-btn>
+            </v-col>
+          </v-row>
+        </v-card>
 
         <v-col cols="12">
           <v-row class="d-flex justify-center">
             <div>
               <v-card
-                class="d-flex rounded justify-center mt-16"
-                elevation="2"
-                height="600px"
+                class="d-flex rounded-xl justify-center mt-16 card"
+                elevation="3"
+                height="350px"
                 width="900px"
+                outlined
               >
-                <v-col cols="6">
-                  <v-row class="d-flex justify-center">
-                    <v-card-title> Bem-vindo a sua Conta </v-card-title>
-                  </v-row>
+                <v-col cols="12">
+                  <v-list-item-subtitle class="d-flex justify-center">
+                    Bem-vindo(a)
+                  </v-list-item-subtitle>
+                  <v-list-item-title
+                    class="d-flex justify-center font-weight-bold name"
+                  >
+                    Jonathan
+                  </v-list-item-title>
+                  <v-col cols="6">
+                    <v-list-item-subtitle> Saldo </v-list-item-subtitle>
+                    <v-list-item-title class="font-weight-bold display-1">
+                      R$&nbsp;0,00
+                    </v-list-item-title>
+                    <v-col cols="12">
+                      <v-btn
+                        class="
+                          d-flex
+                          rounded-xl
+                          mt-10
+                          ml-6
+                          justify-center
+                          bottom
+                        "
+                        outlined
+                        elevation="1"
+                        height="110px"
+                        width="420px"
+                      >
+                        <v-icon x-large color="#ffc107"> mdi-qrcode </v-icon>
+                      </v-btn>
+                    </v-col>
+                  </v-col>
                 </v-col>
               </v-card>
             </div>
@@ -45,5 +77,21 @@
     </v-main>
   </v-app>
 </template>
-                         
+
+<style scoped>
+.name {
+  font-family: Verdana, Geneva, Tahoma, sans-serif;
+}
+
+.bottom {
+}
+
+.card {
+  background-color: ;
+}
+.page {
+  background-color: #f2f2f2;
+}
+</style>
+
 
